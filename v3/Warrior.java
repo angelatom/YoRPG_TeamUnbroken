37ed846ab4e747a9371d0d68006b134186394be8
angelatom <atom@stuy.edu>
2017-11-13 22:15:02 -0500

    Fixed Warrior

--- v3/Warrior.java
+++ v3/Warrior.java
@@ -8,5 +8,19 @@ HW31 -- Ye Olde Role Playing Game, Expanded
 // Subclass warrior will inherit from superclass Protagonist
 public class Warrior extends Protagonist {
 
-    
+    // Constructor that creates warrior and initializes the attributes
+    public Warrior(String warrior) {
+	super();
+	strength += 70;
+	defense += 10;
+	health -= 25;
+	attackRating += .3;
+    }
+
+    // About method that returns a string description
+    // Pre: null
+    // Post: String
+    public static String about() {
+	return "The warrior is an old, faithful class that can be relied on due to its great power in strength" 
+    }
 }
