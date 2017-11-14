644b8b6d2df538a8dcd899e6b071fd0ee445284c
Bing Li <bingli@Bings-Air.home>
2017-11-13 19:23:26 -0500

    added monster subclasses

---
+++ v3/Boss.java
@@ -0,0 +1,23 @@
+/*
+Team Unbroken -- Angela Tom, Bing Li, Jackie Li
+APCS1 pd2
+HW31 -- Ye Olde Role Playing Game Expanded
+2017-11-08
+*/
+
+
+public class Boss extends Monster {
+    private String name;
+
+    public Boss() {
+	name = "Boss";
+	health = 200;
+	strength = (int)((Math.random() * 85) + 20);
+	defense = 40;
+	attackRating = 1.5;
+    }
+
+    public static String about() {
+	return "Oh no! The final boss is here! It is almost impossble to beat because it has elevated all aspects of its traits.";
+    }
+}
